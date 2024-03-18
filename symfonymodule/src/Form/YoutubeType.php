b3adddea50089a82609dcc565195d3555b6a07b0
Mateusz Czosnyka <mateuszczosn@gmail.com>
2024-03-18 22:23:08 +0100

    form validation

--- symfonymodule/src/Form/YoutubeType.php
+++ symfonymodule/src/Form/YoutubeType.php
@@ -8,6 +8,9 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Type;
 use SymfonyModule\Entity\YoutubeComment;
 
 class YoutubeType extends AbstractType
@@ -18,27 +21,75 @@ class YoutubeType extends AbstractType
             ->add('productId', NumberType::class, [
                 'attr' => [
                     'placeholder' => 'The product id'
-                ]
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter product id',
+                    ]),
+                    new Type([
+                        'type' => 'int'
+                    ])
+                ],
             ])
             ->add('customerName', TextType::class, [
                 'attr' => [
                     'placeholder' => 'The customer name'
-                ]
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter customer name',
+                    ]),
+                    new Type([
+                        'type' => 'string'
+                    ]),
+                    new Length([
+                        'max' => 64,
+                        'maxMessage' => "Customer name cannot be longer than {{ limit }} characters"
+                    ])
+                ],
             ])
             ->add('title', TextType::class, [
                 'attr' => [
                     'placeholder' => 'The title'
-                ]
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Type([
+                        'type' => 'string'
+                    ]),
+                    new Length([
+                        'max' => 64,
+                        'maxMessage' => "Title cannot be longer than {{ limit }} characters"
+                    ])
+                ],
             ])
             ->add('content', TextType::class, [
                 'attr' => [
                     'placeholder' => 'The content'
-                ]
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Type([
+                        'type' => 'string'
+                    ]),
+                ],
             ])
             ->add('grade', NumberType::class, [
                 'attr' => [
                     'placeholder' => 'The grade'
-                ]
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Type([
+                        'type' => 'int'
+                    ]),
+                ],
             ])
             ->add('save', SubmitType::class);
     }
